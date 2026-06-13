76bcb93e044cab902e8db9cedc4f4de35da8697f
ndaalia <nidaamaliah.2@gmail.com>
2026-06-13 12:16:11 +0700

    Add files via upload

---
+++ pertemuan_17/latihan4.php
@@ -0,0 +1,25 @@
+<?php
+// Menggunakan password asal-asalan 'rahasia123' yang salah
+$koneksi = mysql_connect("localhost", "root", "rahasia123"); [cite: 42]
+
+if (!$koneksi) {
+    die("Koneksi gagal!");
+}
+?><?php
+// PERBAIKAN 1: Menggunakan mysqli_connect agar mendukung PHP versi baru
+// PERBAIKAN 2: Menggunakan parameter default XAMPP agar tidak "Access Denied" [cite: 176, 181]
+$host     = "localhost";
+$username = "root";
+$password = ""; 
+$database = "pemrograman_web2";
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+// Memeriksa status koneksi database
+if (!$koneksi) {
+    // Jika gagal, tampilkan pesan kesalahannya
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+}
+
+echo "Koneksi database berhasil terhubung!";
+?>
